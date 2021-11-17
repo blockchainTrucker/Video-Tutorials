26132521e46aa1829cbee28b98a13793d8118a32
jwachtel96 <jwachtel96@gmail.com>
2021-11-16 22:29:03 -0500

    11/16

--- controllers/deletePOST.js
+++ controllers/deletePOST.js
@@ -3,12 +3,17 @@ const tutorial = require("../models/Tutorial");
 
 module.exports = function (req, res) {
 	let id = req.params.id;
-	tutorial.findByIdAndRemove(id).then((tutorial) => {
-		res.status(201);
-		res.cookie("status", {
-			type: "success",
-			message: "Tutorial deleted successfully!",
+	tutorial
+		.findByIdAndRemove(id)
+		.then((tutorial) => {
+			res.status(201);
+			res.cookie("status", {
+				type: "success",
+				message: "Tutorial deleted successfully!",
+			});
+			res.redirect("/profile");
+		})
+		.catch((err) => {
+			console.log(err);
 		});
-		res.redirect("/profile");
-	});
 };
