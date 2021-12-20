cc5611c8f08ca34257a4e5e8aec3b5f618b4da7d
Jesse Wachtel <jwachtel96@gmail.com>
2021-12-20 16:16:12 -0500

    12/20

--- controllers/loginPOST.js
+++ controllers/loginPOST.js
@@ -9,47 +9,32 @@ module.exports = function (req, res) {
 	let username = req.body.username;
 	username = username.toLowerCase();
 	let pass = req.body.password;
-	let context = {};
-	user.findOne({ username })
-		.then((user) => {
-			if (user != null) {
-				bcrypt.compare(pass, user.password, (err, result) => {
-					if (result) {
-						res.status(200);
-						let userToken = {
-							id: user._id,
-							username: user.username,
-							firstName: user.firstName,
-							lastName: user.lastName,
-						};
-						const token = jwt.sign(
-							userToken,
-							jwtConfig.secret,
-							jwtConfig.options
-						);
-						res.cookie("user", token);
-						res.cookie("status", {
-							type: "success",
-							message: "User Logged in!",
-						});
-						res.redirect("/");
-					} else {
-						res.status(406);
-						context.loginError = "loginError";
-						context.loginMessage = "Invalid username or password";
-						context.username = username;
-						res.render("login", context);
-					}
-				});
-			} else {
-				res.status(406);
-				context.loginError = "loginError";
-				context.loginMessage = "Invalid username or password";
-				context.username = username;
-				res.render("login", context);
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	user.findOne({ username }).then((user) => {
+		if (user != null) {
+			bcrypt.compare(pass, user.password, (err, result) => {
+				if (result) {
+					res.status(200);
+					let userToken = {
+						id: user._id,
+						username: user.username,
+						firstName: user.firstName,
+						lastName: user.lastName,
+					};
+					const token = jwt.sign(
+						userToken,
+						jwtConfig.secret,
+						jwtConfig.options
+					);
+					res.cookie("user", token);
+					res.cookie("status", {
+						type: "success",
+						message: "User Logged in!",
+					});
+					res.redirect("/");
+				} else {
+					res.status(406);
+				}
+			});
+		}
+	});
 };
